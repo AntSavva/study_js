9dafbc37d1003c8013260e5e967c3d39b025662c
Anton Savva <anton.savvs@gmail.com>
2022-11-22 06:13:19 -0800

    Lesson_1
    obj, connect to html, mass

---
+++ Anton/lessons/js/script.js
@@ -0,0 +1,35 @@
+// let x = 5;
+// const leftBoarderWidth = 10;
+// var name = "Вася";
+
+// x = 10;
+
+// const obj = {
+//     name: "Джон",
+//     age: 25,
+//     isMarridge: false,
+// };
+
+// console.log(obj.age);
+
+// let array = ["Привет", "Миша", "Я Антон", 6, {}, ["Привет", "Катя", "Я Андрей"]];
+
+// console.log(array[5]);
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+// console.log(typeof(answer));  // Узнать тип данных переменной
+
+const massive = [];
+
+
+
+massive[0] = prompt("Как Ваше имя?", '');
+massive[1] = prompt("Ваша фамилия?");
+massive[2] = prompt("Ваш возраст?");
+
+document.write(massive);
+

911374319a86b8a22b5bc4ebe230799845d4b435
Anton Savva <anton.savvs@gmail.com>
2022-11-26 00:50:31 +0700

    HomeWork 3
    Function

---
+++ Anton/lessons/js/function.js
@@ -0,0 +1,30 @@
+let num = 20;
+
+showFirstMessage("Hello world");
+
+function showFirstMessage(text){    //1
+    console.log(text);
+    let num = 10;
+}
+
+let func = function(){
+    console.log("Привет Катя");     //2
+    return ("Петя");
+};
+
+console.log(func());
+
+
+const calc = (a,b) => {
+    return (a + b);                   //3
+};
+
+console.log(calc(5, 12));
+
+
+const str = "test";
+const arr =  [1, 2, 4];
+
+console.log(arr.length);
+
+document.write(str.toUpperCase());
--- Anton/lessons/js/homework.js
+++ Anton/lessons/js/homework.js
@@ -9,30 +9,51 @@ const personalMovieDB = {
 };
 
 
-while (numberOfFilms > 0){
-    let lastFilm = prompt("Введите название фильма");
-    let lastFilmScore = +prompt("Оценка фильма от 0 до 10");
+// while (numberOfFilms > 0){
+//     let lastFilm = prompt("Введите название фильма");
+//     let lastFilmScore = +prompt("Оценка фильма от 0 до 10");
 
-    if (lastFilm && lastFilmScore != "" && lastFilm.length < 50){
-        personalMovieDB.movies[lastFilm] = lastFilmScore;
-        numberOfFilms--;
+//     if (lastFilm && lastFilmScore != "" && lastFilm.length < 50){
+//         personalMovieDB.movies[lastFilm] = lastFilmScore;
+//         numberOfFilms--;
+//     }
+//     else{
+//         alert("Ошибка, введите данные меньше");
+//     }
+// }
+
+
+// if (numberOfFilms<10){
+//     alert("Вы ещё не спец по фильмам");
+// }
+// else if (numberOfFilms<50) {
+//     alert("Вы прогрессируете");
+// }
+// else{
+//     alert("Вы спец по кино");
+// }
+
+
+function showMyDB(check_el){
+    if (check_el == false){
+        console.log(personalMovieDB);
     }
-    else{
-        alert("Ошибка, введите данные меньше");
+    
+};
+
+
+
+function top_genres(){
+
+    for (let i = 1; i<=3;i++){
+        let x = prompt(`Ваш любимый жанр под номером${i}`);
+        personalMovieDB.genres[i] = x;
     }
-}
+    
+};
 
-
-if (numberOfFilms<10){
-    alert("Вы ещё не спец по фильмам");
-}
-else if (numberOfFilms<50) {
-    alert("Вы прогрессируете");
-}
-else{
-    alert("Вы спец по кино");
-}
+showMyDB(personalMovieDB.privat);
+top_genres();
 
 
 
-console.log(personalMovieDB);
